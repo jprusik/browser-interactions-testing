f95fac8a222eafbd48c092ff6dddb823506ab857
Jonathan Prusik <jprusik@classynemesis.com>
2023-06-28 15:39:04 -0400

    re-home autofill testing

--- tests-out/bitwarden/enums/secureNoteType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SecureNoteType = void 0;
-var SecureNoteType;
-(function (SecureNoteType) {
-    SecureNoteType[SecureNoteType["Generic"] = 0] = "Generic";
-})(SecureNoteType = exports.SecureNoteType || (exports.SecureNoteType = {}));
-//# sourceMappingURL=secureNoteType.js.map
--- tests-out/bitwarden/models/view/view.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.View = void 0;
-class View {
-}
-exports.View = View;
-//# sourceMappingURL=view.js.map
