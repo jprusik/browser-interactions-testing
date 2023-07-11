ca75dca1125687cd9a575054a7d94e35a48f669a
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2023-07-11 05:59:04 -0700

    add Vault Management API helper

---
+++ vault-seeder/bw-cli-helper.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# detect if we are in a git repo
+if [ -d .git ]; then
+    # if we are in a git repo, we can use git to find the root of the repo
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+else
+    # if we are not in a git repo, we can use the current script path to find the root of the repo
+    ROOT_DIR=$(dirname "$(realpath "$0")")/..
+fi
+
+# shellcheck source=.env
+source "$ROOT_DIR/.env"
+export BITWARDENCLI_APPDATA_DIR="$ROOT_DIR/vault-seeder/tmp"
+
+mkdir -p "$BITWARDENCLI_APPDATA_DIR"
+
+if ! command -v bw &>/dev/null; then
+    echo "bw could not be found, using dockerized version"
+    export BW_COMMAND="docker run --platform linux/amd64 --rm -it -v $BITWARDENCLI_APPDATA_DIR/tmp:/tmp tangowithfoxtrot/bw-cli:serve-latest bw"
+    BW_COMMAND config server "$SERVER_HOST_URL"
+    BW_COMMAND login "$VAULT_EMAIL" "$VAULT_PASSWORD"
+else
+    bw config server "$SERVER_HOST_URL"
+    bw login "$VAULT_EMAIL" "$VAULT_PASSWORD"
+fi
