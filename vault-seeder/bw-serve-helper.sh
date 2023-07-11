9be40efd0e53f85312bd7938440d3cdb87edb694
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-07-11 11:57:02 -0500

    Update helper to reference local cli

--- vault-seeder/bw-serve-helper.sh
+++ vault-seeder/bw-serve-helper.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
@@ -10,18 +8,20 @@ export BITWARDENCLI_APPDATA_DIR="$ROOT_DIR/vault-seeder/tmp"
 mkdir -p "$BITWARDENCLI_APPDATA_DIR"
 
 BW_COMMAND() {
-    docker run --platform linux/amd64 --rm -it -v "$BITWARDENCLI_APPDATA_DIR:/root/.config/Bitwarden CLI" tangowithfoxtrot/bw-cli:serve-latest bw "$@"
+    # docker run --platform linux/amd64 --rm --network host -it -v "$BITWARDENCLI_APPDATA_DIR:/root/.config/Bitwarden CLI" tangowithfoxtrot/bw-cli:serve-latest bw "$@"
+  bw "$@"
 }
 
-# Use local dev values if not pointed at a hosted environment
-if [ -z "$SERVER_HOST_URL" ]; then
+if [[ -z "${SERVER_HOST_URL:-}" || "$SERVER_HOST_URL" =~ "localhost" || "$SERVER_HOST_URL" =~ "127" ]]; then
     echo "SERVER_HOST_URL is not set, using local dev values"
-    export SERVER_HOST_URL="--api http://localhost:4000 --identity http://localhost:33656 --web-vault https://localhost:8080 --events http://localhost:46273"
+    export SERVER_HOST_URL='--api http://localhost:4000 --identity http://localhost:33656 --web-vault https://localhost:8080 --events http://localhost:46273'
 fi
 
 # Login to the vault
-BW_COMMAND config server "$SERVER_HOST_URL"
+# shellcheck disable=SC2086 # we want to pass the server host url as a single argument
+BW_COMMAND config server $SERVER_HOST_URL || true # no error if already configured
 BW_COMMAND login "$VAULT_EMAIL" "$VAULT_PASSWORD" || true # no error if already logged in
+BW_COMMAND sync || true # no error if already synced
 
 # Start Vault Management API
 pushd "$ROOT_DIR" >/dev/null || exit 1 # ensure the mount points for docker-compose are correct
