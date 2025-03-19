1a7e9d5e3edc9d4568a1f8ce5e97cb75b525d382
Jonathan Prusik <jprusik@users.noreply.github.com>
2025-03-19 09:53:50 -0400

    [PM-19280] Bugfix - Update login fixture for new ui changes (#317)
    * update browser client config and login fixture
    
    * update environment-selector button locator
    
    * Revert "[deps]: Lock file maintenance (#308)"
    
    This reverts commit 747ebe5ac2626d17893b1cd7ef8802ac52ed31fb.
    
    Due to version mismatch in axe-core
    
    * bump playwright version
    
    * update locator

--- tests/fixtures.browser.ts
+++ tests/fixtures.browser.ts
@@ -131,12 +131,29 @@ export const test = base.extend<{
 
     await test.step("Configure the environment", async () => {
       if (vaultHostURL) {
-        const extensionURL = `chrome-extension://${extensionId}/popup/index.html?uilocation=popout#/environment`;
+        const extensionURL = `chrome-extension://${extensionId}/popup/index.html#/login`;
         await testPage.goto(extensionURL, defaultGotoOptions);
-        const baseUrlInput = await testPage.locator("input#baseUrl");
+
+        const environmentSelectorMenuButton = await testPage
+          .locator("environment-selector")
+          .getByRole("button");
+
+        await environmentSelectorMenuButton.waitFor(defaultWaitForOptions);
+        await environmentSelectorMenuButton.click();
+
+        const environmentSelectorMenu = await testPage.getByTestId(
+          "environment-selector-dialog-item-self-hosted",
+        );
+
+        await environmentSelectorMenu.waitFor(defaultWaitForOptions);
+        await environmentSelectorMenu.click();
+
+        const baseUrlInput = await testPage.locator(
+          "input#self_hosted_env_settings_form_input_base_url",
+        );
         await baseUrlInput.waitFor(defaultWaitForOptions);
 
-        await testPage.fill("input#baseUrl", vaultHostURL);
+        await baseUrlInput.fill(vaultHostURL);
 
         await testPage.screenshot({
           fullPage: true,
@@ -147,12 +164,7 @@ export const test = base.extend<{
           ),
         });
 
-        const serverConfigContent = await testPage.locator("#baseUrlHelp");
-        await testPage.click("button[type='submit']");
-        await serverConfigContent.waitFor({
-          ...defaultWaitForOptions,
-          state: "detached",
-        });
+        await testPage.click("bit-dialog button[type='submit']");
       }
     });
 
@@ -175,7 +187,7 @@ export const test = base.extend<{
       await loginButton.waitFor(defaultWaitForOptions);
       await loginButton.click();
 
-      const extensionURL = `chrome-extension://${extensionId}/popup/index.html?uilocation=popout#/tabs/vault`;
+      const extensionURL = `chrome-extension://${extensionId}/popup/index.html#/tabs/vault`;
       await testPage.waitForURL(extensionURL, defaultGotoOptions);
       // Legacy UI
       const vaultFilterBox = await testPage
